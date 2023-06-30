e463c18aec8d42947ca0d4ef510a35d1c8579f83
Harshsukhija24 <harshsukhija2002@gmail.com>
2023-06-30 22:40:45 +0530

    first commit

---
+++ src/components/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB3QR2faJQYiiHMidS5qA4wjnLesS-ojs",
+  authDomain: "chatapp-eb0e4.firebaseapp.com",
+  projectId: "chatapp-eb0e4",
+  storageBucket: "chatapp-eb0e4.appspot.com",
+  messagingSenderId: "360981633275",
+  appId: "1:360981633275:web:78992ed48ae778df8d385e"
+};
+
+ export const app = initializeApp(firebaseConfig);
